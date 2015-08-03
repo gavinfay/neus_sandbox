702c713f473d81c35f84cce208413f17fd7e2cff
Gavin Fay <gfay42@gmail.com>
2015-08-03 16:25:34 -0400

    changes to at harvest file, working version, 1

--- prm_change.r
+++ prm_change.r
@@ -612,6 +612,126 @@ change.prm4 <- function(path,origfile,param.name,outfile,mapfile,param.type,
 }
 
 
+change.prm4b <- function(path,origfile,param.name,outfile,mapfile,param.type,
+                         name2=NULL,mapfile2=NULL,ogfile1="NeusGroups.csv",
+                         ogfile2=NULL)
+{
+  ###### generic version that uses 2 maps
+  # 1 for the variable names,
+  # 2 for the vector length
+  
+  library(stringr)
+  #param.name <- "pPREY1"
+  #name2 <- "1"
+  #set working directory
+  setwd(path)
+  #Lookup table with new codes for functional groups along with 'parent' groups from Atlantis-neus v1.
+  CodeRelations <- read.csv(mapfile)
+  CodeRelations2 <- read.csv(mapfile2)
+  oldg1 <- read.csv(ogfile1)
+  oldg2 <- read.csv(ogfile2)
+  
+  #read in orig param file
+  TheData <- read.table(origfile,col.names=1:100,comment.char="",fill=TRUE,header=FALSE)
+  
+  #additional columns in pPREY matrix
+  
+  old.codes.2 <- as.character(oldg2$Code)
+  mapvals <- match(CodeRelations2$Parent,oldg2$Code)
+  #mapvals2 <- c(mapvals,63:65)
+  mapvals2 <- mapvals
+  
+  #find the length-weight parameters from the old prm file, store them
+  if (param.name!= "") 
+    pick <- grep(param.name,TheData[,1])
+  if (length(name2)>0) pick <-pick[grep(name2,TheData[pick,1])]
+  if (param.name== "") pick <- grep(name2,TheData[,1])
+  xx <- TheData[pick,]
+  pick2 <- grep('#',xx[,1])
+  if(length(pick2)>0) xx <- xx[-pick2,]
+  yy <- TheData[pick+1,]
+  if(length(pick2)>0) yy <- yy[-pick2,]
+  
+  tempmat <- matrix(NA,nrow=nrow(xx),ncol=2)
+  if (param.name!="")
+  {
+    for (igroup in 1:nrow(tempmat)) tempmat[igroup,1] <- 
+      substr(xx[igroup,1],start=1+str_length(param.name),
+             stop=str_length(xx[igroup,1]))
+    #strsplit(as.character(xx[igroup,1]),param.name)[[1]][2]
+    if (length(name2)>0)
+    {
+      for (igroup in 1:nrow(tempmat)) tempmat[igroup,1] <- 
+        #strsplit(as.character(tempmat[igroup,1]),name2)[[1]][1]
+        substr(tempmat[igroup,1],stop=str_length(tempmat[igroup,1])-
+                 str_length(name2),start=1)
+    }
+  }
+  if (param.name=="")
+  {
+    for (igroup in 1:nrow(tempmat)) tempmat[igroup,1] <- 
+      strsplit(as.character(xx[igroup,1]),name2)[[1]][1]
+  }     
+  ###if (length(name2)>0) for (igroup in 1:nrow(tempmat)) tempmat[igroup,1] <- strsplit(tempmat[igroup,1],name2)[[1]][1]
+  tempmat[,2] <- as.numeric(as.character(xx[,2]))
+  ##pick <- grep("li_b_",TheData[,1])
+  ##tempmat[,3] <- as.numeric(as.character(TheData[pick,2]))
+  
+  
+  #as.numeric(t(yy[1,1:10]))
+  
+  #assign the old vals to the new codes, write a csv for data entry with columns to change highlighted and 'parent' group parameter values inserted.
+  output <- CodeRelations #list(Code=CodeRelations$Child,Parent=CodeRelations$Parent,Change=CodeRelations$Change)
+  output$x <- rep(NA,nrow(output))
+  #output$x <- tempmat[match(CodeRelations$Parent,tempmat[,1]),2]
+  output$x <- nrow(CodeRelations2)
+  
+  #output$x[] <- 11
+  #output$li_b <- rep(NA,nrow(output))
+  #output$li_b <- tempmat[match(CodeRelations$Parent,tempmat[,1]),3]
+  #output$source <- ""
+  #output$source[output$Change==0] <- "Atlantis-Neus v1"
+  #head(output)
+  #write.table(output,file="weightlength.csv",col.names=TRUE,row.names=FALSE,quote=FALSE,sep=",")
+  
+  matches <- match(CodeRelations$Parent,tempmat[,1])
+  match2 <- match(CodeRelations2$Parent,old.codes.2)
+  #numcols <- unique(as.numeric(output$x[which(is.na(output$x)==FALSE)]))
+  numcols <- nrow(CodeRelations2)
+  output2 <- matrix(NA,ncol=numcols,nrow=nrow(output))
+  for (irow in 1:nrow(output2))
+  {
+    output2[irow,] <-  
+      as.numeric(as.character(t(yy[matches[irow],match2])))
+  }
+  
+  ## assuming that the new values have been filled in, put back into format for the .prm file
+  if (param.type==2)  #all groups
+  {
+    Ngroups <- nrow(CodeRelations)
+    temptab <- matrix(NA,nrow=Ngroups,ncol=2)
+    temptab[,1] <- paste(param.name,output$Child,sep="")
+    if (length(name2)>0) temptab[,1] <- paste(param.name,output$Child,name2,sep="")
+    temptab[,2] <- output$x
+    #temptab[,3] <- paste("weight at length a parameter for ",output[output$IsVertebrate==1,"Long.Name"],sep="")
+    write(" ",file=outfile)
+    rows.use <- 1:nrow(temptab)
+    for (row in rows.use)
+    {
+      irow = which(rows.use==row)
+      if (is.na(output2[row,1])==FALSE)
+      {
+        write.table(t(temptab[irow,]),file=outfile,col.names=FALSE,row.names=FALSE,
+                    quote=FALSE,sep=" ",append=TRUE)
+        write.table(t(output2[irow,]),file=outfile,col.names=FALSE,row.names=FALSE,
+                    quote=FALSE,sep=" ",append=TRUE)
+        write(" ",file=outfile,append=TRUE)
+      }
+    }
+  }
+  
+  
+}
 
 
 library(stringr)
@@ -636,15 +756,25 @@ change.prm5(path,origfile,param.name,outfile,mapfile,param.type,name2)
 
 
 ########### now do some stuff for the harvest file
+library(stringr)
 setwd("~/Atlantis/neus_sandbox")
 path <- getwd()
+mapfile2 <- "FleetMaps_v15.csv"
 mapfile <- "coderelations.csv"
+ogfile2 <- "NeusFisheries.csv"
+ogfile1 <- "NeusGroups.csv"
+
 origfile <- "at_harvest_neus_v15_DE.prm"
-param.name <- "EPlant_"
-name2 <- "_T15"
-param.type=3
+outfile <- "newtable.out"
+param.name <- "flaghabitat_"
+name2 <- "day"
+param.type <- 2
+change.prm(path,origfile,param.name,outfile,mapfile,param.type,name2)
+change.prm3(path,origfile,param.name,outfile,mapfile,param.type,name2)
 
 
+change.prm4b(path,origfile,param.name,outfile,mapfile,param.type,name2,
+             mapfile2,ogfile1,ogfile2)
 
 
 
